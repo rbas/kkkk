f10330834508a13c561903bd1e46b5be3be97a3a
rbas <rbas.cz@gmail.com>
2016-04-29 00:04:00 +0200

    Update

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 openssl aes-256-cbc -K $encrypted_248b4e8cf39a_key -iv $encrypted_248b4e8cf39a_iv -in id_rsa.enc -out id_rsab -d
-
+chmod 600 id_rsab
 ssh app@alpha-node-2.rosti.cz -p 11106 -o 'StrictHostKeyChecking no' -i id_rsab 'echo "blah" >> /srv/app/deploy.log'
 
 
-ls -alh
+ls -alh ../
+
