111db6c50cdb5b9448fad212f180df32ca18f7c3
rbas <rbas.cz@gmail.com>
2016-04-29 11:16:44 +0200

    Update

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,5 @@ tar -czf build.tgz $BUILD_NAME
 
 #ssh app@alpha-node-2.rosti.cz -p 11106 -o 'StrictHostKeyChecking no' 'echo "blah" >> /srv/app/deploy.log'
 scp -P 11106 -o 'StrictHostKeyChecking no' build.tgz app@alpha-node-2.rosti.cz:/srv/
-ssh app@alpha-node-2.rosti.cz -p 11106 -o 'StrictHostKeyChecking no' 'tar xfz /srv/build.tgz && /srv/$(BUILD_NAME)/install.sh $(BUILD_NAME)'
+ssh app@alpha-node-2.rosti.cz -p 11106 -o 'StrictHostKeyChecking no' "tar xfz /srv/build.tgz && /srv/${BUILD_NAME}/install.sh ${BUILD_NAME}"
 
