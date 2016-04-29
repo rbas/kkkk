37864807d41311b2f9b38de25c0c2c841c2d1067
rbas <rbas.cz@gmail.com>
2016-04-29 15:00:12 +0200

    Update

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-BUILD_NAME=$(date +"%Y-%m-%d-%y-%H:%M.%N")
+BUILD_NAME=build-$(date +"%Y-%m-%d-%y-%H:%M.%N")
 
 mkdir $BUILD_NAME
 mv * $BUILD_NAME
 tar -czf build.tgz $BUILD_NAME
 
-#ssh app@alpha-node-2.rosti.cz -p 11106 -o 'StrictHostKeyChecking no' 'echo "blah" >> /srv/app/deploy.log'
 scp -P 11106 -o 'StrictHostKeyChecking no' build.tgz app@alpha-node-2.rosti.cz:/srv/
 ssh app@alpha-node-2.rosti.cz -p 11106 -o 'StrictHostKeyChecking no' "tar xfz /srv/build.tgz && /srv/${BUILD_NAME}/install.sh ${BUILD_NAME}"
-
--- install.sh
+++ install.sh
@@ -1,4 +1,20 @@
 #!/usr/bin/env bash
 
+remove_old_builds() {
+	MAX_BUILDS=5
+	BUILD_LIST=$(ls|grep "build-" |sort -r)
+
+	COUNTER=0
+	for f in $BUILD_LIST
+	do
+	  ((COUNTER = COUNTER + 1))
+	  if [ $COUNTER -gt $MAX_BUILDS ]; then
+		rm -rf $f
+	  fi
+	done
+}
+
 unlink /srv/app && ln -s /srv/$1 /srv/app
 supervisorctl restart app
+
+remove_old_builds
