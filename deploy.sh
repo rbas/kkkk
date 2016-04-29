08cef9a6c49c8fc17d239aa7b458dbe17ce11ef9
rbas <rbas.cz@gmail.com>
2016-04-29 10:19:49 +0200

    Update

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+mkdir build
+mv * build
+tar -czf build.tgz build
+
+ls -alh ~/.ssh/
+
 ssh app@alpha-node-2.rosti.cz -p 11106 -o 'StrictHostKeyChecking no' 'echo "blah" >> /srv/app/deploy.log'
 
 
